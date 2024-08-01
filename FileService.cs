dd56e876015f5af2cb11c44b3b746baebf5080e8
Deekshith <81959275+Deekshith-07@users.noreply.github.com>
2024-08-01 16:55:39 +0530

    Initial Commit

---
+++ FileService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOrganizer
+{
+    public class FileService
+    {
+        public string? DirPath { get; set; }
+
+        public void ArrangeFiles(string[] files)
+        {
+            List<List<string>> processedFiles = IdentifyFileType(files);
+
+            foreach (var file in processedFiles)
+            {
+                try
+                {
+                    if (!string.IsNullOrEmpty(DirPath))
+                    {
+                        string targetDirectory = Path.Combine(DirPath, file[1]);
+
+                        if (!Directory.Exists(targetDirectory))
+                        {
+                            Directory.CreateDirectory(targetDirectory);
+                            Console.WriteLine($"Folder created: {file[1]}");
+                        }
+
+                        string targetFilePath = Path.Combine(targetDirectory, Path.GetFileName(file[0]));
+
+                     
+                        if (File.Exists(targetFilePath))
+                        {
+                            string newFileName = Path.GetFileNameWithoutExtension(file[0]) + "_copy" + Path.GetExtension(file[0]);
+                            targetFilePath = Path.Combine(targetDirectory, newFileName);
+                        }
+
+                        File.Move(file[0], targetFilePath);
+                        Console.WriteLine($"Successfully moved {file[0]} to {file[1]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Directory path is not set.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        private List<List<string>> IdentifyFileType(string[] files)
+        {
+            List<List<string>> fileDetails = new();
+
+            foreach (var file in files)
+            {
+                if (File.Exists(file))
+                {
+                    string fileExtension = Path.GetExtension(file).TrimStart('.');
+                    List<string> fileInfo = new()
+                            {
+                                file,
+                                fileExtension
+                            };
+                    fileDetails.Add(fileInfo);
+                }
+            }
+            return fileDetails;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FileOrganizer 
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            bool validInput =false;
+            FileService fileService = new();
+
+            while (!validInput)
+            {
+                Console.Write("Enter a directory path: ");
+                string? dirPath = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(dirPath))
+                {
+                    Console.WriteLine("Directory path can't be empty or null");
+                    continue;
+                }
+
+                try
+                {
+                    string[] files = Directory.GetFiles(dirPath);
+                    validInput = true;
+                    fileService.DirPath = dirPath;
+                    fileService.ArrangeFiles(files);
+                    
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    Console.WriteLine("\nDirectory not found. Please enter valid path\n");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"\nError occurred: {ex.Message}\n");
+                }
+            }
+        }
+    }
+}
